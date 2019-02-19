08fe3c4f33cdcd0215b36a37d86a4b5ae92cda4a
shreeka1 <shreekanth_venkateshappa@mcafee.com>
2019-02-18 19:01:04 -0800

    styles and click event handling

--- test-proj/src/app/hw/hw.component.ts
+++ test-proj/src/app/hw/hw.component.ts
@@ -10,6 +10,20 @@ export class HwComponent implements OnInit {
 
   msg: string = "hi"
   msg2: any;
+  toggle_var: boolean = true;
+  toggle_caption: string = "Hide"
+
+  toggle_fun() {
+          if(this.toggle_var) {
+                  this.toggle_caption = "Show";
+          }
+          else {
+                  this.toggle_caption = "Hide";
+          }
+
+          this.toggle_var = !this.toggle_var;
+  }
+
   @Input('param') param: string;
   @Input('obj') obj: Object;
   constructor() {
